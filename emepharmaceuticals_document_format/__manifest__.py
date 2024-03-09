f1f880e489afab2722e3a91850f842e39c83a2d1
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-03-09 02:37:22 +0100

    eme documents

---
+++ emepharmaceuticals_document_format/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Eme Pharmaceuticals Document Format",
+    "summary": "Eme Pharmaceuticals Document Format",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "author": "Manuel Calero - Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        "base",
+        "purchase",
+    ],
+    "data": [
+        "report/purchase_quotation_templates.xml",
+    ],
+    "installable": True,
+}
